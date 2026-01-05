bfc27a8029d36855408542fc6eba9757c931f78f
Egehan Kılıçarslan <56479018+EgehanKilicarslan@users.noreply.github.com>
2026-01-05 05:18:52 +0100

    Add files via upload

---
+++ .config/fish/conf.d/010-keybindings.fish
@@ -0,0 +1,6 @@
+# ------------------------------------------------------------------------------
+# Key Bindings Configuration
+# ------------------------------------------------------------------------------
+
+# Erase the universal fish_key_bindings variable
+set --erase --universal fish_key_bindings
---
+++ .config/fish/conf.d/020-theme.fish
@@ -0,0 +1,50 @@
+# ------------------------------------------------------------------------------
+# Theme Colors Configuration
+# ------------------------------------------------------------------------------
+
+set --global fish_color_autosuggestion '#6a737d'
+set --global fish_color_black '#3b4252'
+set --global fish_color_blue '#5e81ac'
+set --global fish_color_brblack '#4c566a'
+set --global fish_color_brblue '#5e81ac'
+set --global fish_color_brcyan '#88c0d0'
+set --global fish_color_brgreen '#a3be8c'
+set --global fish_color_brmagenta '#b48ead'
+set --global fish_color_brred '#bf616a'
+set --global fish_color_brwhite '#ffffff'
+set --global fish_color_bryellow '#ebcb8b'
+set --global fish_color_cancel -r
+set --global fish_color_command '#88c0d0'
+set --global fish_color_comment '#6a737d'
+set --global fish_color_cwd '#81a1c1'
+set --global fish_color_cwd_root '#bf616a'
+set --global fish_color_cyan '#8fbcbb'
+set --global fish_color_end '#C78EFF'
+set --global fish_color_error '#bf616a'
+set --global fish_color_escape '#ebcb8b'
+set --global fish_color_green '#a3be8c'
+set --global fish_color_history_current '#88c0d0'
+set --global fish_color_host normal
+set --global fish_color_host_remote yellow
+set --global fish_color_magenta '#b48ead'
+set --global fish_color_normal '#d8dee9'
+set --global fish_color_operator '#81a1c1'
+set --global fish_color_pager_completion '#d8dee9'
+set --global fish_color_pager_prefix '#81a1c1'
+set --global fish_color_pager_progress '--background=#434c5e'
+set --global fish_color_param '#e5e9f0'
+set --global fish_color_quote '#a3be8c'
+set --global fish_color_red '#bf616a'
+set --global fish_color_redirection '#FF6E6E'
+set --global fish_color_search_match '--background=#4c566a'
+set --global fish_color_selection '--background=#4c566a'
+set --global fish_color_status red
+set --global fish_color_user brgreen
+set --global fish_color_valid_path --underline
+set --global fish_color_white '#eceff4'
+set --global fish_color_yellow '#ebcb8b'
+set --global fish_pager_color_completion normal
+set --global fish_pager_color_description yellow -i
+set --global fish_pager_color_prefix normal --bold --underline
+set --global fish_pager_color_progress brwhite --background=cyan
+set --global fish_pager_color_selected_background -r
